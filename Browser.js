cd5913ba7bc539837bf5f6979d5ca86d4fb64000
Andrew S <andrews54757@gmail.com>
2017-01-25 19:04:18 -0500

    Update Browser.js

--- Browser.js
+++ Browser.js
@@ -1,1 +1,78 @@
+ var BinReader = function (buf) {
 
+        this.index = 0;
+        this.buffer = new DataView(buf);
+    }
+    BinReader.prototype.readString8 = function () {
+        var data = "";
+        while (this.index <= this.buffer.byteLength) {
+            var d = this.readUInt8();
+            if (!d) break;
+            data += String.fromCharCode(d);
+        }
+        return data;
+    }
+    BinReader.prototype.readString16 = function () {
+        var data = "";
+        while (this.index <= this.buffer.byteLength) {
+            var d = this.readUInt16BE();
+            if (!d) break;
+            data += String.fromCharCode(d);
+        }
+        return data;
+    }
+    BinReader.prototype.readString32 = function () {
+        var data = "";
+        while (this.index <= this.buffer.byteLength) {
+            var d = this.readUInt32BE();
+            if (!d) break;
+            data += String.fromCharCode(d);
+        }
+        return data;
+    }
+    BinReader.prototype.readInt8 = function () {
+        return this.buffer.getInt8(this.index++);
+    }
+    BinReader.prototype.readUInt8 = function () {
+        return this.buffer.getUint8(this.index++);
+    }
+    BinReader.prototype.readInt16BE = function () {
+        var data = this.buffer.getInt16(this.index);
+        this.index += 2;
+        return data;
+    }
+    BinReader.prototype.readInt16LE = function () {
+        var data = this.buffer.getInt16(this.index, true);
+        this.index += 2;
+        return data;
+    }
+    BinReader.prototype.readUInt16BE = function () {
+        var data = this.buffer.getUint16(this.index);
+        this.index += 2;
+        return data;
+    }
+    BinReader.prototype.readUInt16LE = function () {
+        var data = this.buffer.getUint16(this.index, true);
+        this.index += 2;
+        return data;
+    }
+    BinReader.prototype.readInt32BE = function () {
+        var data = this.buffer.getInt32(this.index);
+        this.index += 4;
+        return data;
+    }
+    BinReader.prototype.readInt32LE = function () {
+        var data = this.buffer.getInt32(this.index, true);
+        this.index += 4;
+        return data;
+    }
+    BinReader.prototype.readUInt32BE = function () {
+        var data = this.buffer.getUint32(this.index);
+        this.index += 4;
+        return data;
+    }
+    BinReader.prototype.readUInt32LE = function () {
+        var data = this.buffer.getUint32(this.index, true);
+        this.index += 4;
+        return data;
+    }
